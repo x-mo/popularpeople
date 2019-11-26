5f680896d09c5d162905a2ba94d153131c159bac
X <X_MohamedAhmed@yahoo.com>
2019-11-27 00:32:19 +0200

    Profile Images Grid migrated to PersonDetailsActivity

--- app/src/main/java/com/x/popularpeople/ui/person_details/PersonDetailsActivity.kt
+++ app/src/main/java/com/x/popularpeople/ui/person_details/PersonDetailsActivity.kt
@@ -4,25 +4,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
 import com.bumptech.glide.Glide
 import com.x.popularpeople.R
-import com.x.popularpeople.api.ORIGINAL_POSTER_BASE_URL
 import com.x.popularpeople.api.POSTER_BASE_URL
 import com.x.popularpeople.api.TheMovieDBClient
 import com.x.popularpeople.api.TheMovieDBInterface
 import com.x.popularpeople.model.PersonDetails
 import com.x.popularpeople.repository.NetworkState
+import com.x.popularpeople.ui.profiles.ProfilesListAdapter
+import com.x.popularpeople.ui.profiles.ProfilesListRepository
+import com.x.popularpeople.ui.profiles.ProfilesViewModel
 import kotlinx.android.synthetic.main.activity_person_details.*
+import kotlinx.android.synthetic.main.activity_person_details.rv_profiles
+import kotlinx.android.synthetic.main.activity_profiles.*
 
 class PersonDetailsActivity : AppCompatActivity() {
 
     private lateinit var viewModel: PersonDetailsViewModel
     private lateinit var personDetailsRepository: PersonDetailsRepository
+
+    private lateinit var profilesViewModel: ProfilesViewModel
+    private lateinit var profilesListRepository: ProfilesListRepository
+
+    val profilesAdapter = ProfilesListAdapter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +42,7 @@ class PersonDetailsActivity : AppCompatActivity() {
         val apiService: TheMovieDBInterface = TheMovieDBClient.getClient()
         personDetailsRepository = PersonDetailsRepository(apiService)
 
-        viewModel = getViewModel(personId)
+        viewModel = getPersonViewModel(personId)
 
         viewModel.personDetails.observe(this, Observer {
             bindUI(it)
@@ -45,6 +54,29 @@ class PersonDetailsActivity : AppCompatActivity() {
             txt_error.visibility = if (it == NetworkState.ERROR) View.VISIBLE else View.GONE
 
         })
+
+        profilesListRepository = ProfilesListRepository(apiService)
+
+        profilesViewModel = getProfilesViewModel(personId)
+
+        profilesViewModel.profilesList.observe(this, Observer {
+            profilesAdapter.submitList(it.profiles)
+
+            Log.d("RV", "Bound" + it.profiles[0].filePath)
+
+        })
+
+        setupRV()
+
+    }
+
+    fun setupRV() {
+
+        val gridLayoutManager = GridLayoutManager(this, 4)
+
+        rv_profiles.layoutManager = gridLayoutManager
+        rv_profiles.setHasFixedSize(false)
+        rv_profiles.adapter = profilesAdapter
     }
 
     private fun bindUI(it: PersonDetails) {
@@ -65,11 +97,19 @@ class PersonDetailsActivity : AppCompatActivity() {
 
     }
 
-    private fun getViewModel(personId: Int): PersonDetailsViewModel {
+    private fun getPersonViewModel(personId: Int): PersonDetailsViewModel {
         return ViewModelProviders.of(this, object : ViewModelProvider.Factory {
             override fun <T : ViewModel?> create(modelClass: Class<T>): T {
                 return PersonDetailsViewModel(personDetailsRepository, personId) as T
             }
         })[PersonDetailsViewModel::class.java]
+    }
+
+    private fun getProfilesViewModel(personId: Int): ProfilesViewModel {
+        return ViewModelProviders.of(this, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return ProfilesViewModel(profilesListRepository, personId) as T
+            }
+        })[ProfilesViewModel::class.java]
     }
 }
--- app/src/main/java/com/x/popularpeople/ui/profiles/ProfilesActivity.kt
+++ app/src/main/java/com/x/popularpeople/ui/profiles/ProfilesActivity.kt
@@ -18,75 +18,48 @@ import kotlinx.android.synthetic.main.activity_profiles.*
 
 class ProfilesActivity : AppCompatActivity() {
 
-    private lateinit var profilesViewModel: ProfilesViewModel
-    private lateinit var profilesListRepository: ProfilesListRepository
-
+//    private lateinit var profilesViewModel: ProfilesViewModel
+//    private lateinit var profilesListRepository: ProfilesListRepository
+//
+//    val profilesAdapter = ProfilesListAdapter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profiles)
 
 
-        val personId: Int = 1245
-
-        val apiService: TheMovieDBInterface = TheMovieDBClient.getClient()
-        profilesListRepository = ProfilesListRepository(apiService)
-
-        profilesViewModel = getViewModel(personId)
-
-        profilesViewModel.profilesList.observe(this, Observer {
-            bindUI(it)
-
-        })
-
-
-        setupRV()
-
+//        val personId: Int = 1245
+//
+//        val apiService: TheMovieDBInterface = TheMovieDBClient.getClient()
+//        profilesListRepository = ProfilesListRepository(apiService)
+//
+//        profilesViewModel = getViewModel(personId)
+//
+//        profilesViewModel.profilesList.observe(this, Observer {
+//            profilesAdapter.submitList(it.profiles)
+//
+//        })
+//
+//        setupRV()
 
     }
 
-    val profilesAdapter = ProfilesListAdapter(this)
-    fun setupRV(){
 
+//    fun setupRV(){
+//
+//        val gridLayoutManager = GridLayoutManager(this, 4)
+//
+//        rv_profiles.layoutManager = gridLayoutManager
+//        rv_profiles.setHasFixedSize(true)
+//        rv_profiles.adapter = profilesAdapter
+//    }
 
-        val gridLayoutManager = GridLayoutManager(this, 4)
-
-//        gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
-//            override fun getSpanSize(position: Int): Int {
-//                val viewType: Int = profilesAdapter.getItemViewType(position)
-//                return if (viewType == profilesAdapter.PEOPLE_VIEW_TYPE) 1
-//                else 2
+//    private fun getViewModel(personId: Int): ProfilesViewModel {
+//        return ViewModelProviders.of(this, object : ViewModelProvider.Factory {
+//            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+//                return ProfilesViewModel(profilesListRepository, personId) as T
 //            }
-//        }
-
-        rv_profiles.layoutManager = gridLayoutManager
-        rv_profiles.setHasFixedSize(true)
-        rv_profiles.adapter = profilesAdapter
-    }
-
-
-    private fun bindUI(it: Profiles) {
-        Log.d("BindUI", "Arrived")
-        Log.d("BindUI", it.profiles[0].filePath)
-        Log.d("BindUI", it.profiles[1].filePath)
-        Log.d("BindUI", it.profiles[2].filePath)
-
-        profilesAdapter.submitList(it.profiles)
-
-//        val personPosterURL: String = POSTER_BASE_URL + it.profilePath
-//        Glide.with(this)
-//            .load(personPosterURL)
-//            .into(iv_profile_image)
-//        Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
-
-    }
-
-    private fun getViewModel(personId: Int): ProfilesViewModel {
-        return ViewModelProviders.of(this, object : ViewModelProvider.Factory {
-            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-                return ProfilesViewModel(profilesListRepository, personId) as T
-            }
-        })[ProfilesViewModel::class.java]
-    }
+//        })[ProfilesViewModel::class.java]
+//    }
 
 }
