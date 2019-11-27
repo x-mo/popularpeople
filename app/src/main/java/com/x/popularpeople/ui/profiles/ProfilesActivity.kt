7d805f70f6db5c38df0b32728d0b5064bb99db0f
X <X_MohamedAhmed@yahoo.com>
2019-11-27 02:27:40 +0200

    Download photo to device

--- app/src/main/java/com/x/popularpeople/ui/profiles/ProfilesActivity.kt
+++ app/src/main/java/com/x/popularpeople/ui/profiles/ProfilesActivity.kt
@@ -1,8 +1,18 @@
 package com.x.popularpeople.ui.profiles
 
+import android.Manifest
+import android.app.DownloadManager
+import android.app.DownloadManager.Request.NETWORK_MOBILE
+import android.app.DownloadManager.Request.NETWORK_WIFI
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -20,14 +30,72 @@ import kotlinx.android.synthetic.main.activity_profiles.*
 
 class ProfilesActivity : AppCompatActivity() {
 
+    private val STORAGE_PERMISSION_CODE: Int = 1000
+
+    var imagePath: String? = ORIGINAL_POSTER_BASE_URL
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profiles)
 
-        val imagePath: String? = ORIGINAL_POSTER_BASE_URL + intent.getStringExtra("path")
-
+        imagePath += intent.getStringExtra("path")
         Glide.with(this).load(imagePath).into(profile_image)
 
+        save_to_device.setOnClickListener {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    requestPermissions(
+                        arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                        STORAGE_PERMISSION_CODE
+                    )
+                } else {
+                    //download
+                    downloadFile()
+                }
+            } else {
+                //download
+                downloadFile()
+            }
+        }
+
+    }
+
+    private fun downloadFile() {
+
+        val request = DownloadManager.Request(Uri.parse(imagePath))
+        request.setAllowedNetworkTypes(NETWORK_WIFI or NETWORK_MOBILE)
+        request.setTitle("PopularPeople")
+        request.setDescription("File is downloading...")
+
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        request.setDestinationInExternalPublicDir(
+            Environment.DIRECTORY_DOWNLOADS,
+            "${System.currentTimeMillis()}"
+        )
+
+        val manager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        manager.enqueue(request)
+
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            STORAGE_PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    downloadFile()
+                } else {
+                    Toast.makeText(this, "Permission needed to save photo", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/com/x/popularpeople/utils/SaveImage.kt
@@ -0,0 +1,8 @@
+package com.x.popularpeople.utils
+
+import android.content.Context
+
+class SaveImage (context: Context){
+
+
+}
